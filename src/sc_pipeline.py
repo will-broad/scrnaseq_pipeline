1419156f0d0da4577d0aa1aa2574eeca18a1ec6f
dan-broad <dchafamo@broadinstitute.org>
2022-01-29 08:53:17 -0500

    KCO-003 | automate pipeline

--- src/sc_pipeline.py
+++ src/sc_pipeline.py
@@ -5,19 +5,26 @@ import pandas as pd
 import concurrent.futures
 import steps
 import threading
+import os
 
-project_name = "Gut_eQTL"
-sample_tracking_file = os.getcwd() + "/sampletracking_guteqtl_rerun.csv"
-gcp_basedir = "gs://fc-secure-1620151c-e00c-456d-9daf-4d222e1cab18/Gut_eQTL"
-email = "dchafamo@broadinstitute.org"
-alto_workspace = "'kco-tech/Gut_eQTL'"
+"""
+Config Section - Modify this section only
+"""
+project_name = os.getenv("PROJECT_NAME", default="Gut_eQTL")
+sample_tracking_file = os.getenv("SAMPLE_TRACKING_FILE", default="sampletracking_guteqtl_rerun.csv")
+gcp_basedir = os.getenv("GCP_BUCKET_BASEDIR", default="gs://fc-secure-1620151c-e00c-456d-9daf-4d222e1cab18/Gut_eQTL")
+email = os.getenv("EMAIL", default="dchafamo@broadinstitute.org")
+alto_workspace = os.getenv("TERRA_WORKSPACE", default="'kco-tech/Gut_eQTL'")
+
+
+"""
+Set global variables + Preprocess Sample tracking file
+"""
 cellranger_version = "6.0.1"
 max_parallel_threads = 30
-
 count_matrix_name = "raw_feature_bc_matrix.h5"
 filtered_matrix_name = "filtered_feature_bc_matrix.h5"
 cellbender_matrix_name = "out_FPR_0.01_filtered.h5"
-
 cwd = os.getcwd()
 basedir = cwd + "/" + project_name + "/sc_processed"
 os.makedirs(basedir, exist_ok=True)
@@ -28,9 +35,14 @@ master_tracking['seq_dir'] = master_tracking['seq_dir'].apply(lambda sd: sd[:-1]
 seq_dirs = set(master_tracking[master_tracking.run_pipeline]['seq_dir'])
 buckets = build_buckets(gcp_basedir, project)
 alto_dirs = build_alto_folders(buckets)
+log_file = os.getenv("PIPELINE_LOGS", default='{}/{}.log'.format(basedir, project_name))
 
 
-def process_sample(seq_dir):
+def process_flowcell(seq_dir):
+    """
+    Initiate pipeline for a set of samples within a single Flowcell.
+    :param seq_dir: GCP Cloud Storage link to raw BCL directory
+    """
     sample_tracking = master_tracking[master_tracking.run_pipeline &
                                       (master_tracking.seq_dir == seq_dir)]
 
@@ -63,8 +75,7 @@ def process_sample(seq_dir):
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)-7s | %(threadName)-15s | %(levelname)-5s | %(message)s",
-                        level=logging.INFO, datefmt="%m-%d %H:%M", filename='{}/{}.log'.format(basedir, project_name),
-                        filemode='w')
+                        level=logging.INFO, datefmt="%m-%d %H:%M", filename=log_file, filemode='w')
 
     start_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     logging.info("Running scRNASeq pipeline for project {} on {}".format(project, start_time))
@@ -74,4 +85,4 @@ if __name__ == "__main__":
     logging.info("Master sample tracking file: \n\n {} \n".format(master_tracking.to_markdown()))
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_parallel_threads) as executor:
-        executor.map(process_sample, seq_dirs)
+        executor.map(process_flowcell, seq_dirs)
