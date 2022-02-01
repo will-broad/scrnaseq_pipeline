a726167445c19dae8566771f7d0d2041de6f9601
dan-broad <dchafamo@broadinstitute.org>
2022-02-01 12:29:09 -0500

    KCO-003 | automate pipeline

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,28 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Usage:
 #  bash run.sh --project-name "scp-test" \
 #   --gcp-bucket-basedir "gs://fc-secure-1620151c-e00c-456d-9daf-4d222e1cab18/scp-test" \
 #   --sample-tracking-file "gs://fc-secure-1620151c-e00c-456d-9daf-4d222e1cab18/scp-test/sample_tracking_small.csv" \
 #   --email "dchafamo@broadinstitute.org" \
-#   --workspace "'kco-tech/Gut_eQTL'"
+#   --workspace "'kco-tech/Gut_eQTL'" \
+#   --count-matrix-name "raw_feature_bc_matrix.h5"
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    --project-name=*)
-      project_name="${1#*=}"
+    --project-name)
+      project_name="$2"
       ;;
-    --gcp-bucket-basedir=*)
-      gcp_bucket_basedir="${1#*=}"
+    --gcp-bucket-basedir)
+      gcp_bucket_basedir="$2"
       ;;
-    --sample-tracking-file=*)
-      sample_tracking_file="${1#*=}"
+    --sample-tracking-file)
+      sample_tracking_file="$2"
       ;;
-    --email=*)
-      email="${1#*=}"
+    --email)
+      email="$2"
       ;;
-    --workspace=*)
-      workspace="${1#*=}"
+    --workspace)
+      workspace="$2"
+      ;;
+    --count-matrix-name)
+      count_matrix_name="$2"
       ;;
     *)
       printf "***************************\n"
@@ -31,11 +35,11 @@ while [ $# -gt 0 ]; do
       exit 1
   esac
   shift
+  shift
 done
 
-pip install --upgrade dsub
 
-gcloud auth configure-docker
+#gcloud auth configure-docker
 
 dsub --provider google-cls-v2 --project "microbiome-xavier" --regions us-east1 \
   --service-account "scrnaseq-pipeline@microbiome-xavier.iam.gserviceaccount.com" \
@@ -47,4 +51,5 @@ dsub --provider google-cls-v2 --project "microbiome-xavier" --regions us-east1 \
   --env PROJECT_NAME="$project_name" \
   --env GCP_BUCKET_BASEDIR="$gcp_bucket_basedir" \
   --env EMAIL="$email" \
-  --env TERRA_WORKSPACE="$workspace"
+  --env TERRA_WORKSPACE="$workspace" \
+  --env COUNT_MATRIX_NAME="${count_matrix_name}"
