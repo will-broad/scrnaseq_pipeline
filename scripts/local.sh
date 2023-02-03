51cf44fb3a6437ea1be88a7ae039418cdd46f873
Will Hwang <will@broadinstitute.org>
2023-02-03 15:58:49 +0000

    cellbender

--- scripts/local.sh
+++ scripts/local.sh
@@ -10,14 +10,14 @@
 
 # conda activate dsub_env
 
-dir_name="tutorial"
-gcp_bucket_basedir="gs://fc-secure-15bf93cd-d43c-4a70-b7de-0ee36bf3a52a/${dir_name}"
-sample_tracking_file="${gcp_bucket_basedir}/sampletracking_small.csv"
-project_name="tutorial"
-email="dchafamo@broadinstitute.org"
-workspace="'kco-tech/sc_pipeline_tutorial'"
+dir_name="finngen_gex"
+gcp_bucket_basedir="gs://fc-secure-d4adbbf9-8265-4a5c-b14f-23a5f1b5c4f9/${dir_name}"
+sample_tracking_file="${gcp_bucket_basedir}/012523_batch2_gex_sampletracker.csv"
+project_name="finngen_gex"
+email="will@broadinstitute.org"
+workspace="'693-finland-v2f/Finngen'"
 count_matrix_name="raw_feature_bc_matrix.h5"
-steps="MKFASTQ,COUNT,CUMULUS,CELLBENDER,CELLBENDER_CUMULUS"
+steps="CELLBENDER,CELLBENDER_CUMULUS"
 mkfastq_memory="120G"
 mkfastq_diskspace="1500"
 cellranger_method="broadinstitute:cumulus:Cellranger:2.1.1"
@@ -33,7 +33,7 @@ dsub --provider google-cls-v2 --project "microbiome-xavier" --regions us-east1 \
   --service-account "scrnaseq-pipeline@microbiome-xavier.iam.gserviceaccount.com" \
   --image "gcr.io/microbiome-xavier/conda-alto" --disk-size '10' --timeout '2d'\
   --logging "$gcp_bucket_basedir/logs/" \
-  --command "wget https://github.com/dan-broad/scrnaseq_pipeline/archive/master.zip && unzip master.zip && cd scrnaseq_pipeline-master/src && python sc_pipeline.py" \
+  --command "wget https://github.com/will-broad/scrnaseq_pipeline/archive/gex_atac.zip && unzip gex_atac.zip && cd scrnaseq_pipeline-gex_atac/src && python sc_pipeline.py" \
   --output PIPELINE_LOGS="$gcp_bucket_basedir/logs/execution_$current_time.log" \
   --input SAMPLE_TRACKING_FILE="$sample_tracking_file" \
   --env PROJECT_NAME="$project_name" \
