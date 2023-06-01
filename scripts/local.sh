1491cc6220c22c5628e5f9c00e0ca2c686c10abf
Will Hwang <will@broadinstitute.org>
2023-06-01 00:04:26 +0000

    batch3 novaseqX count

--- scripts/local.sh
+++ scripts/local.sh
@@ -12,12 +12,12 @@
 
 dir_name="finngen_gex"
 gcp_bucket_basedir="gs://fc-secure-d4adbbf9-8265-4a5c-b14f-23a5f1b5c4f9/${dir_name}"
-sample_tracking_file="${gcp_bucket_basedir}/052323_batch3_gex_sampletracker.csv"
+sample_tracking_file="${gcp_bucket_basedir}/053123_batch3_gex_count_sampletracker.csv"
 project_name="finngen_gex"
 email="will@broadinstitute.org"
 workspace="'693-finland-v2f/Finngen'"
 count_matrix_name="raw_feature_bc_matrix.h5"
-steps="MKFASTQ,COUNT,CELLBENDER,CELLBENDER_CUMULUS"
+steps="COUNT,CELLBENDER,CELLBENDER_CUMULUS"
 mkfastq_memory="256G"
 mkfastq_diskspace="2500"
 cellranger_method="broadinstitute:cumulus:Cellranger:2.2.0"
@@ -25,7 +25,7 @@ cumulus_method="broadinstitute:cumulus:cumulus:2.1.1"
 cellbender_method="cellbender/remove-background/11"
 cellranger_version="7.0.1"
 cellranger_atac_version="2.1.0"
-cellranger_arc_version="2.0.2"
+cellranger_arc_version="2.0.1"
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
