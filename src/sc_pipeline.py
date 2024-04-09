3ea86780405e686155ca81b2339c859ab91add2a
Will Hwang <will@broadinstitute.org>
2024-04-09 13:52:51 +0000

    cb

--- src/sc_pipeline.py
+++ src/sc_pipeline.py
@@ -11,7 +11,7 @@ import os
 Config Section - Modify this section only
 """
 project_name = os.getenv("PROJECT_NAME", default="finngen_gex")
-sample_tracking_file = os.getenv("SAMPLE_TRACKING_FILE", default="../data/031224_batch4_gex_count_sampletracker.csv")
+sample_tracking_file = os.getenv("SAMPLE_TRACKING_FILE", default="../data/040824_batch4_gex_count_sampletracker.csv")
 gcp_basedir = os.getenv("GCP_BUCKET_BASEDIR", default="gs://fc-secure-d4adbbf9-8265-4a5c-b14f-23a5f1b5c4f9/finngen_gex")
 email = os.getenv("EMAIL", default="will@broadinstitute.org")
 alto_workspace = os.getenv("TERRA_WORKSPACE", default="'693-finland-v2f/Finngen'")
